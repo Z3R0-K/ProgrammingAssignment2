f0a6a448c461f7c142c348fd97bca73f66db9082
Z3R0-K <okc_rapid@MartinRuterKing.fritz.box>
2021-01-05 10:17:51 +0100

    This is the completed R Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix intially sets and gets the new matrix, that is going to be inversed. 
+## The setsolve function then inverts the the matrix "x" and assigns it to m.
+## The new value of m is then got by the getsolve function.
+## Basically, it works analog to the maekVector function, which was provided as
+## as an example.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      
+      set <- function(y){
+        x <<- y
+        m <<- NULL
+      }
+      
+      get <- function() x
+      setsolve <- function(solve) m <<- solve (x)
+      getsolve <- function() m
+      
+      list(set = set, get = get, setsolve = setsolve, 
+           getsolve  = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function first gets m.
+## The if loop checks whether m is not null, so the inverse matrix has
+## already been assigned to m. If this is the case, it will just return m.
+## If  m is equal to null, cacheSolve will get the current value of x,
+## calculate its inverse and then set m to the inverse of x, before it returns
+## m.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  
+  if(is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+    
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+    
 }
